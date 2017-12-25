e5f16218560b0be6d1ede4160f4629bf5372cf7e
DeadMansMarch <lsp425@gmail.com>
2017-12-24 20:03:21 -0500

    Initial Commit

---
+++ The Witness/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Witness
+//
+//  Created by Liam Pierce on 12/24/17.
+//  Copyright © 2017 Virtual Earth. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
